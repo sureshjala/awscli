7ff1e1ab762d0e0bbb9ed3b563742001bb15498f
sureshjala <113531639+sureshjala@users.noreply.github.com>
2022-10-27 17:34:27 +0530

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -1,3 +1,1 @@
-aws ec2 create-vpc \
-    --cidr-block 10.0.0.0/16 \
-    --tag-specification ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]
+aws ec2 create-vpc --cidr-block 10.0.1.0/16 --tag-specification ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]
